4db0118ad2ce6062280eb2d18e8f505e8d072917
Coleman Hooper <chooper@firesim2.Millennium.Berkeley.EDU>
2023-02-03 18:58:17 -0800

    test

--- examples/pytorch/translation/run_finetuning_from_scratch.sh
+++ examples/pytorch/translation/run_finetuning_from_scratch.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES=0,3
+export CUDA_VISIBLE_DEVICES=0
 python run_finetuning_from_scratch.py \
     --model_name_or_path t5-small \
     --config_name ./config_12_2.json \
     --do_train \
     --do_eval \
-    --source_lang en \
-    --target_lang de \
+    --source_lang de \
+    --target_lang en \
     --dataset_name iwslt2017 \
     --dataset_config_name iwslt2017-de-en \
     --output_dir /scratch/chooper/iwslt/tst-translation \
@@ -14,4 +14,5 @@ python run_finetuning_from_scratch.py \
     --per_device_eval_batch_size=48 \
     --overwrite_output_dir \
     --cache_dir /scratch/chooper/iwslt/cache-translation \
+    --num_train_epochs 50 \
     --predict_with_generate
